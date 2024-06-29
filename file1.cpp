028ebffe7b0595d17d6e65cd38d06abdf398fd35
NavyaNelluri <navyachowdary.nelluri@slu.edu>
2024-06-28 23:15:34 -0500

    edited file1 in main branch at this time

--- file1.cpp
+++ file1.cpp
@@ -1,3 +1,7 @@
 Hi
 Hello
+<<<<<<< HEAD
 Cherry pick content commited and edited at this time
+=======
+Cherry pick content commited and edited
+>>>>>>> aa6dfd8 (edited and commited file1 in main branch)
